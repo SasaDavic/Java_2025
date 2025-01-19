1321f041c67e35c0934ba95ecbe26d2589006121
Sasa Davic <sasadavic.nis@gmail.com>
2025-01-19 10:30:23 +0100

    predavanja 16.08.

--- src/p16_08_2022_zadatak3/Auto.java
+++ src/p16_08_2022_zadatak3/Auto.java
@@ -2,7 +2,7 @@ package p16_08_2022_zadatak3;
 
 public class Auto {
 
-	
+
 	public String imeVlasnika;
 	public String marka;
 	public int brVrata;
@@ -13,21 +13,25 @@ public class Auto {
 	public int mesecIstekaRegistracije;
 	public int kubikaza;
 	
+	public String brRegistracije;
+	public boolean klimaOn;
+	
+	public int maxBrzina;
+	
+	public double kapacitetRezervoara;
+	public double trenutnoGorivaURezervoaru;
 	
 	public void stampaj() {
 		System.out.println(this.imeVlasnika);
 		System.out.println(this.marka + " - " + this.brVrata + "-ro vrata");
 		System.out.println("Sa potrosnjom od " + this.potrosnjaNa100Km + " l na 100km");
 		System.out.println(this.trenutnBrzina + " km/h je trenutna brzina.");
+		System.out.println("Registracija: " + this.brRegistracije);
 		System.out.println("Godina proizvodnje: " + this.godProizvodnje);
 		if (jeOldtimer() == true) {
-			System.out.println("Auto je oldtimer!");
+			System.out.println("Vozilo je oldtimer!");
 		}
-		if (vaziRegistracija(6) == true) {
-			System.out.println("Registracija nije istekla!");
-		} else {
-			System.out.println("Registracije je istekla, cena registracije je: " + cenaRegistracije());
-		}
+		System.out.println("Potrosnja vozila u ovom trenutku je: " + potrosnja());
 	}
 
 	public boolean jePrekoracio (int ogranicenje) {
@@ -61,6 +65,49 @@ public class Auto {
 			return this.kubikaza * 100;
 		} else {
 			return this.kubikaza * 100 * 1.3;
+		}
+	}
+	public void dodajGas() {
+		this.trenutnBrzina += 10;
+		if (this.trenutnBrzina > this.maxBrzina) {
+			this.trenutnBrzina = this.maxBrzina;
+		}
+	}
+	public void koci() {
+		this.trenutnBrzina -= 10;
+		if (this.trenutnBrzina < 0) {
+			this.trenutnBrzina = 0;
+		}
+	}
+	public double potrosnja() {
+		double x = this.trenutnBrzina * 0.01  * this.potrosnjaNa100Km;
+		if (this.klimaOn == true) {
+			return x * 1.2;
+		} else 
+			return x;
+	}
+	public void stampajTablu() {
+		int brCrtica = (this.trenutnBrzina * 100) / this.maxBrzina;
+		for (int i = 0; i < 100; i++) {
+			if (i < brCrtica) {
+				System.out.print("|");
+			} else {
+				System.out.print(".");
+			}
+		}
+		System.out.println(this.trenutnBrzina + "/" + this.maxBrzina + " km/h");
+	}
+	
+	public double natoci (double kolikoLitra) {
+		if (this.kapacitetRezervoara >= this.trenutnoGorivaURezervoaru + kolikoLitra) {
+			double a = kolikoLitra * 170;
+			System.out.println("Natocili ste " + kolikoLitra + "l, to je " + a + "din.");
+			return a;
+		} else {
+			double b = this.kapacitetRezervoara - this.trenutnoGorivaURezervoaru;
+			double c = b * 170;
+			System.out.println("Rezervoar je pun! Natocili ste " + b + "l, to je " + c + "din.");
+			return b;
 		}
 	}
 	
--- src/p16_08_2022_zadatak3/Zadatak3.java
+++ src/p16_08_2022_zadatak3/Zadatak3.java
@@ -42,9 +42,42 @@ metodu koja vraca da li je istekla registracije. Metoda kao parametar prima tren
 metodu koja racuna i vraca cenu registracije za auto. Za automobile do 2000 kubika cena registracije kubikaza * 100din, 
 za automobile preko 2000 kubika dodatno se uracunava 30% na cenu.
 
+(Dopuna 3)
+Dopuniti klasu Auto tako da ima:
+atribut broj registracije
+da li je ukljucena klima u autu
+metodu dodajGas, koja povecava trenutnu brzinu za 10.
+metodu koci, koja smanjuje brzinu za 10. Brzina ne moze da ode ispod nule.
+metodu koja racuna i vraca trenutnu potrosnju auta. Metoda racuna po formuli:
+faktor klime - ako je ukljucena klima faktor je 1.2, ako nije ukljucena onda je 1.0
+(trenutna brzina / 100.0 * potrosnja na 100km) * faktor klime
+
+(Dopuna 4)
+Dopuniti klasu Auto tako da ima:
+atribut za maksimalnu brzinu na auto (npr: 240)
+Promenite metodu dodajGas tako da povecanje brzine ne bude preko maksimalne brzine auta
+metodu stampajTablu koja stampa brzinomer sa table. Brzinomer uvek ima 100 jedinica za prikaz brzine.
+Ako je npr maksimalna brzina 240 i auto je u trenutnoj brzini 100, metoda stampa 
+               |||||||||||||||||||||||||||||||||||||||||..................................................................... 100/240km/h
+(objasnjenje: ovde imamo 41 crticu koja oznacava brzinu 100km/h i imamo 59 tackica koje oznacavaju ostatak do maksimalne brzine)
+Broj crtica se racuna po formuli: (trenutna brzina * 100) / maksimalna brzina.
+
+(Dopuna 5)
+Dopuniti klasu Auto tako da ima:
+kapacitet rezervoara
+trenutnu kolicinu goriva u rezervoaru (u litrima)
+metodu natoci gorivo, metoda prima kao parametar litrazu goriva koja se toci a vraca novcanu vrednost goriva. Litar goriva je 170din. 
+Vodite racuna da trenutna kolicina goriva ne predje kapacitet rezervoara, takodje ukoliko se unese kolicina koja prelazi kapacitet, 
+tu kolicinu koja prelazi ne racunate u cenu.
+
+
 	 */
+		int mesecSada = 5;
+		int ogranicenje = 60;
 		
 		Auto a1 = new Auto();
+		a1.kapacitetRezervoara = 40;
+		a1.trenutnoGorivaURezervoaru = 35;
 		a1.brVrata = 5;
 		a1.imeVlasnika = "Marko Nikolic";
 		a1.marka = "BMW";
@@ -53,11 +86,27 @@ za automobile preko 2000 kubika dodatno se uracunava 30% na cenu.
 		a1.godProizvodnje = 1945;
 		a1.kubikaza = 3000;
 		a1.mesecIstekaRegistracije = 10;
+		a1.brRegistracije = "NI987-25";
+		a1.klimaOn = true;
+		a1.maxBrzina = 240;
 		a1.stampaj();
 		
-		System.out.println("Vozac " + a1.imeVlasnika + " treba da plati kaznu " + a1.kazna(60));
+		if (a1.kazna(ogranicenje) != 0) {
+			System.out.println("Vozac " + a1.imeVlasnika + 
+					" je prekoracio dozvoljenu brzinu, treba da plati kaznu " + a1.kazna(ogranicenje));
+		}
+		if (a1.vaziRegistracija(mesecSada) == false) {
+			System.out.println("Vozilu je istekla registracija!!! Cena nove registracije je " + a1.cenaRegistracije());
+		}
+		a1.stampajTablu();
+		a1.koci();
+		a1.stampajTablu();
+		a1.natoci(3);
 		System.out.println();
+		
 		Auto a2 = new Auto();
+		a2.kapacitetRezervoara = 25;
+		a2.trenutnoGorivaURezervoaru = 22;
 		a2.brVrata = 3;
 		a2.imeVlasnika = "Ivana Jovic";
 		a2.marka = "Yugo";
@@ -65,10 +114,27 @@ za automobile preko 2000 kubika dodatno se uracunava 30% na cenu.
 		a2.trenutnBrzina = 50;
 		a2.kubikaza = 1800;
 		a2.godProizvodnje = 1980;
-		a2.mesecIstekaRegistracije = 5;
+		a2.mesecIstekaRegistracije = 4;
+		a2.brRegistracije = "BG789-21";
+		a2.klimaOn = false;
+		a2.maxBrzina = 120;
 		a2.stampaj();
 		
-		System.out.println("Vozac " + a2.imeVlasnika + " treba da plati kaznu " + a2.kazna(60));
+		if (a2.kazna(ogranicenje) != 0) {
+			System.out.println("Vozac " + a2.imeVlasnika + 
+					" je prekoracio dozvoljenu brzinu, treba da plati kaznu " + a2.kazna(ogranicenje));
+		}
+		if (a2.vaziRegistracija(mesecSada) == false) {
+			System.out.println("Vozilu je istekla registracija!!! Cena nove registracije je " + a2.cenaRegistracije());
+		}
+		a2.stampajTablu();
+		a2.dodajGas();
+		a2.dodajGas();
+		a2.stampajTablu();
+		a2.natoci(5);
+		
+		
+		
 	}
 
 }
